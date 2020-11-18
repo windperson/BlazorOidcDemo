600d28887aa9847e09da147a261be0ee4d874885
Chen-Yu Pao <windperson@gmail.com>
2020-11-18 18:05:46 +0800

    Add project files.

---
+++ BlazorOidcDemo/Pages/_HostAuthModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorOidcDemo.Pages
+{
+    public class _HostAuthModel : PageModel
+    {
+        public IActionResult OnGetLogin()
+        {
+            return Challenge(AuthProps(), "oidc");
+        }
+
+        public async Task OnGetLogout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc", AuthProps());
+        }
+
+        private AuthenticationProperties AuthProps()
+        {
+            return new AuthenticationProperties { RedirectUri = Url.Content("~/") };
+        }
+    }
+}
